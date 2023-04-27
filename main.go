7d7667469d1598bc7104be4115b780ab96e6a492
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-27 17:02:34 +0430

    Add postAlbums

--- main.go
+++ main.go
@@ -13,18 +13,25 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+var albums []album
 
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	err := c.BindJSON(&newAlbum)
+	if err != nil {
+		return
+	}
+	albums = append(albums, newAlbum)
+	c.Status(http.StatusCreated)
 }
