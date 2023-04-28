9c9bba6060039d7382908318fd32438647092fe6
Mostafa Negim <mostafa.negim@gmail.com>
2023-04-28 22:39:31 +0430

    Add updateBook

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"reflect"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Book struct {
-	ID     string  `json:"id"`
+	ID     int     `json:"id"`
 	Title  string  `json:"title"`
 	Author string  `json:"author"`
 	Price  float64 `json:"price"`
@@ -20,6 +23,7 @@ func main() {
 	router.GET("/books", getBooks)
 	router.POST("/books", postBooks)
 	router.GET("/books/:id", getBook)
+	router.PATCH("/books/:id", updateBook)
 	router.DELETE("/books/:id", deleteBook)
 	router.Run("localhost:8080")
 }
@@ -38,8 +42,15 @@ func postBooks(c *gin.Context) {
 	c.Status(http.StatusCreated)
 }
 
-func getBook(c *gin.Context)  {
-	id := c.Param("id")
+func getBook(c *gin.Context) {
+	ids := c.Param("id")
+	id, err := strconv.Atoi(ids)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+
 	for _, b := range books {
 		if id == b.ID {
 			c.IndentedJSON(http.StatusOK, b)
@@ -48,13 +59,44 @@ func getBook(c *gin.Context)  {
 	c.Status(http.StatusNotFound)
 }
 
-func deleteBook(c *gin.Context)  {
-	id := c.Param("id")
+func deleteBook(c *gin.Context) {
+	ids := c.Param("id")
+	id, err := strconv.Atoi(ids)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
 	for i, b := range books {
 		if id == b.ID {
-			books = append(books[:i],books[i+1:]...)
+			books = append(books[:i], books[i+1:]...)
 			break
 		}
 	}
 	c.Status(http.StatusNoContent)
+}
+
+func updateBook(c *gin.Context) {
+	ids := c.Param("id")
+	id, err := strconv.Atoi(ids)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+	var newBook Book
+	if err := c.BindJSON(&newBook); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+	nb := reflect.ValueOf(newBook)
+	typeOfS := nb.Type()
+	for _, b := range books {
+		if b.ID == id {
+			for i := 0; i < nb.NumField(); i++ {
+				fmt.Printf("Field: %s\tValue: %v\n", typeOfS.Field(i).Name, nb.Field(i).Interface())
+			}
+		}
+	}
 }
